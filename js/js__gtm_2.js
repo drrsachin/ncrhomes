21871a3c9d8ebcf1aa77a394974b9aac96e76d3a
drrsachin <52072991+drrsachin@users.noreply.github.com>
2022-01-20 21:45:50 +0530

    Create js__gtm_2.js

---
+++ js/js__gtm_2.js
@@ -0,0 +1,1 @@
+document.write( '<noscript><iframe src=\'https://www.googletagmanager.com/ns.html?id=GTM-53H2JBV\' height=\'0\' width=\'0\' style=\'display:none;visibility:hidden\'></iframe></noscript>' );
